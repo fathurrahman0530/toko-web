ca9929f488cab64e3922b197683078efacc17356
FATHUR RAHMAN ABDULLAH <63894938+fathurrahman0530@users.noreply.github.com>
2022-08-03 22:15:58 +0800

    update api

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,12 @@
 <?php
 
+use App\Http\Controllers\ProductApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/products', [ProductApiController::class, 'index']);
+Route::get('/product/create', [ProductApiController::class, 'create']);
+Route::post('/products', [ProductApiController::class, 'store']);
+Route::get('/product/{id}', [ProductApiController::class, 'show']);
+Route::put('/product/{id}', [ProductApiController::class, 'update']);
+Route::delete('/product/{id}', [ProductApiController::class, 'destroy']);
